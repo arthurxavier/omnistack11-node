de3bdd4a8db25c749f39dc36583c809797ee0522
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-03-29 05:34:27 -0300

    Adding DELETE route incidents

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -19,5 +19,28 @@ module.exports = {
     });
 
     return reponse.json({ id });
+  },
+
+  async delete(request, response) {
+    const { id } = request.params;
+    const ong_id = request.headers.authorization;
+
+    if (await ongs_havent_incidents(id, ong_id)) {
+      return response.status(401).json({ error: "Operation not permitted." });
+    }
+
+    await connection("incidents")
+      .where("id", id)
+      .delete();
+
+    return response.status(204).send();
   }
 };
+
+async function ongs_havent_incidents(id, ong_id) {
+  const incidents = await connection("incidents")
+    .where("id", id)
+    .select("ong_id")
+    .first();
+  return incidents.ong_id !== ong_id ? true : false;
+}
