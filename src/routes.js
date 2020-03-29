0af6809614a001663b2c27dd37c1ac738acc9e39
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-03-29 03:43:13 -0300

    Adding POST route to ongs create with id random by crypto

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const crypto = require("crypto");
 
 // Decoupling the route module express to a variable.
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({
-    evento: "omnistack 11.0",
-    autor: "Arthur Xavier"
-  });
+routes.post("/ongs", (request, response) => {
+  const { name, email, whatsapp, cirty, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  console.log(id);
+
+  return response.json();
 });
 
 module.exports = routes;
