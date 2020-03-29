3e181102a05a0ef9b85c54284cf2fe923f6c56f2
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-03-29 04:55:33 -0300

    Adding Incident Controller with create and index Adding Incident Contrtoller in routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
 const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
 
 // Decoupling the route module express to a variable.
 const routes = express.Router();
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
 
 module.exports = routes;
