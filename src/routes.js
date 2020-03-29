006266fe7a5dea1df25830f258758d2f3d430584
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-03-29 05:41:24 -0300

    Adding GET profile route to list incidents of specific ong

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
 
 // Decoupling the route module express to a variable.
 const routes = express.Router();
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+routes.get("/profile", ProfileController.index);
 
 routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.create);
