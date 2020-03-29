74a94596fdad05218d5a7fb913e7b65b78a103f2
arthurxavier <arthurxaviergiffoni@gmail.com>
2020-03-29 05:53:11 -0300

    Adding route session controller by id ongs

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ const express = require("express");
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
 const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 // Decoupling the route module express to a variable.
 const routes = express.Router();
+
+routes.post("/sessions", SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
